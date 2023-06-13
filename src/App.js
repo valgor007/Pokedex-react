3b4bea5cbf6b65f42dcad0087db9c6471c79b51c
valgor007 <rafaupup@gmail.com>
2023-06-12 22:51:47 -0600

    tutorial terminado, lista pokemon con axios.get

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
+import Pagination from "./Pagination";
+
+function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setcurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setnextPageURl] = useState();
+  const [prevPageUrl, setprevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setnextPageURl(res.data.next);
+        setprevPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+      });
+  }, [currentPageUrl]);
+
+  function gotoNextPage() {
+    setcurrentPageUrl(nextPageUrl);
+  }
+  function gotoPrevPage() {
+    setcurrentPageUrl(prevPageUrl);
+  }
+
+  if (loading) return "Loading...";
+  return (
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        gotoNextPage={nextPageUrl ? gotoNextPage : null}
+        gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
+      />
+    </>
+  );
+}
+
+export default App;
